b9b24b883bd4b421a525e2918ca2d19c78f2617f
Cat Plus Plus <me@catpp.eu>
2019-07-26 21:03:45 +0200

    Shaders.

--- Emulator/data/shader_fragment.glsl
+++ Emulator/data/shader_fragment.glsl
@@ -1,1 +1,13 @@
 ﻿#version 460 core
+
+layout(location = 0) in vec2 inUV;
+layout(location = 1) in vec4 inColor;
+
+layout(location = 0) uniform sampler2D inSampler;
+
+layout(location = 0) out vec4 outColor;
+
+void main()
+{
+	outColor = texture2D(inSampler, inUV);
+}
